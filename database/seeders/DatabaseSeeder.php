e231f298805bba2cb58267490dfc6a123bf0431c
MichaleXIV <maulana.saputra1909@gmail.com>
2025-08-14 15:28:49 +0700

    feat: seeder for master zonasi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Zonasi;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +16,25 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $zonasiList = [
+            "Zona Industri",
+            "Zona Sarana Pelayanan Kawasan",
+            "Zona Pariwisata",
+            "Zona Logistik",
+            "Zona Ruang Terbuka Hijau",
+        ];
 
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
         ]);
+
+
+        foreach ($zonasiList as $zonasi) {
+            Zonasi::create([
+                'nama' => $zonasi,
+            ]);
+        };
     }
 }
